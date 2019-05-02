a8d321d5cebae972d1d25271d63a016f01c71b90
Gabriel Rocha <gabrielscrocha4@gmail.com>
2019-05-02 07:44:08 -0300

    f

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:listacompras/pages/home.dart';
+import 'package:listacompras/pages/root-page.dart';
+import 'package:listacompras/services/authentication.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String _email, _password;
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      key: _formKey,
-      body: Form(
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-              validator: (v) {
-                if (v.isEmpty) {
-                  return 'E-mail não pode ser vazio';
-                }
-              },
-              onSaved: (v) => {_email = v},
-              decoration: InputDecoration(labelText: 'E-mail'),
-            ),
-            TextFormField(
-              validator: (v) {
-                if (v.length < 6) {
-                  return 'Sua senha precisa ter 6 caracteres';
-                }
-              },
-              onSaved: (v) => {_password = v},
-              decoration: InputDecoration(labelText: 'Senha'),
-              obscureText: true,
-            ),
-            RaisedButton(
-              onPressed: logar,
-              child: Text('Logar'),
-            )
-          ],
+    return new MaterialApp(
+        title: 'Flutter login demo',
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
         ),
-      ),
-    );
-  }
-
-  Future<void> logar() async {
-    final formState = _formKey.currentState;
-    if (formState.validate()) {
-      formState.save();
-      try {
-        FirebaseUser user = await FirebaseAuth.instance
-            .signInWithEmailAndPassword(email: _email, password: _password);
-        Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
-      } catch (e) {
-        print(e.message);
-      }
-    }
+        home: new RootPage(auth: new Auth()));
   }
 }
